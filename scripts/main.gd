0a19313931aaaff5325a6fd5059dd68d72a89afd
Vorgen <148902444+Vorgenstad@users.noreply.github.com>
2023-10-26 09:38:09 +0200

    Added music and sound effect

--- scripts/main.gd
+++ scripts/main.gd
@@ -16,12 +16,17 @@ func new_game():
 	
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	
+	$Music.play()
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	
 	$HUD.show_game_over()
+	
+	$Music.stop()
+	$DeathSound.play()
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
